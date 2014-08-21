34530a26bf4e0f08d84819e484ca4134c71ae27a
Paul M Furley <paul@paulfurley.com>
2014-08-21 17:18:58 +0100

    Load recipients from recipients.csv
    Rather than settings.sh
    
    This allows for subscribers to be added with other metadata about them,
    rather than just their number.

--- festivalweather/texter.py
+++ festivalweather/texter.py
@@ -1,6 +1,10 @@
 from __future__ import unicode_literals
 
+import csv
 import os
+import re
+
+from os.path import dirname, join as pjoin
 
 from twilio.rest import TwilioRestClient
 
@@ -9,13 +13,51 @@ def send_sms(message):
     account = os.environ['TWILIO_SID']
     token = os.environ['TWILIO_AUTH_TOKEN']
     from_number = os.environ['TWILIO_FROM_NUMBER']
-    recipients = os.environ['RECIPIENT_NUMBERS'].split(',')
+    dry_run = os.environ.get('DRY_RUN', 'false') == 'true'
 
     client = TwilioRestClient(account=account, token=token)
 
-    for recipient in filter(None, recipients):
+    for recipient in get_recipients():
+        if dry_run:
+            print("Dry run, skipping {}".format(recipient))
+            continue
+
         client.messages.create(
             from_=from_number,
             to=recipient,
             body=message
         )
+
+
+def get_recipients():
+    filename = pjoin(dirname(__file__), '..', 'recipients.csv')
+    with open(filename, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if row['enabled'] != 'true':
+                continue
+
+            mobile_number = row['mobile_number']
+            if not valid_mobile_number(mobile_number):
+                print("ERROR: Bad mobile number '{}'".format(mobile_number))
+                continue
+
+            print("{} ({}): {}".format(
+                row['name'],
+                row['paypal_email'],
+                mobile_number))
+
+            yield mobile_number
+
+
+def valid_mobile_number(mobile_number):
+    """
+    >>> valid_mobile_number('+447987123765')
+    True
+    >>> valid_mobile_number('+44798712376')
+    False
+    >>> valid_mobile_number('+4479871237657')
+    False
+    """
+    match = re.match(r'^\+447\d{9}$', mobile_number)
+    return match is not None
